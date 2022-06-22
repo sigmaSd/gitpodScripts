e0ad55b40745218af0077868ebd1825316827a71
Nbiba Bedis <bedisnbiba@gmail.com>
2022-06-22 12:13:26 +0100

    Update to ds 0.6.0 + bug fixes

--- deps.ts
+++ deps.ts
@@ -1,1 +1,6 @@
-export { $ } from "https://deno.land/x/simple_shell@0.5.0/mod.ts";
+export {
+  $,
+  $t,
+  simpleRun,
+} from "https://deno.land/x/simple_shell@0.6.0/mod.ts";
+export {} from "https://deno.land/x/simple_shell@0.6.0/src/stringUtils.ts";
--- mold.ts
+++ mold.ts
@@ -1,4 +1,4 @@
-import { $ } from "./deps.ts";
+import { $, $t, simpleRun } from "./deps.ts";
 
 export const installMold = async () => {
   const releasePage = await fetch(
@@ -8,38 +8,25 @@ export const installMold = async () => {
   const moldUrl = "https://github.com" +
     releasePage.match(/href="(.*mold-.*-x86_64-linux.tar.gz)"/)![1];
 
-  $`wget -O mold.tar.gz ${moldUrl}`;
-  $`tar -xzf mold.tar.gz`;
-  $`rm mold.tar.gz`;
-  $`mv mold-*-x86_64-linux/ mold`;
+  $t`wget -O mold.tar.gz ${moldUrl}`;
+  $t`tar -xzf mold.tar.gz`;
+  $t`rm mold.tar.gz`;
+  simpleRun("sh -c mv mold-*-x86_64-linux mold");
 
-  $`mkdir .cargo`;
+  $`mkdir .cargo`; // ignore exists error
 
   // cargo and cargo.toml are both valid
   // check if there is one already and use it
   let cargoConfig;
-  if (pathExist("./cargo/config")) {
+  if ("./cargo/config".pathExists()) {
     cargoConfig = "./cargo/config";
   } else {
     // default to cargo.toml
     cargoConfig = "./cargo/config.toml";
   }
 
-  $`echo 
+  $t`echo 
 '[target.x86_64-unknown-linux-gnu]
 linker = "clang"
 rustflags = ["-C", "link-arg=-fuse-ld=/workspace/deno/mold/bin/mold"]' >> ${cargoConfig}`;
 };
-
-function pathExist(filePath: string): boolean {
-  try {
-    Deno.statSync(filePath);
-    return true;
-  } catch (e) {
-    if (e instanceof Deno.errors.NotFound) {
-      return false;
-    } else {
-      throw e;
-    }
-  }
-}

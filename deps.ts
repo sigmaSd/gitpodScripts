6a3d46a95ae6195181d2d2f931a50231f15ae104
Nbiba Bedis <bedisnbiba@gmail.com>
2022-08-09 21:46:29 +0100

    Update ds

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export {
   $$,
   $t,
   shellRun,
-} from "https://deno.land/x/simple_shell@0.9.0/mod.ts";
-export * as pathUtils from "https://deno.land/x/simple_shell@0.9.0/src/stringUtils.ts";
+} from "https://deno.land/x/simple_shell@0.13.0/mod.ts";
+export * as stringUtils from "https://deno.land/x/simple_shell@0.13.0/src/utils/string.ts";

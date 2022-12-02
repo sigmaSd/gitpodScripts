2f4500ae16b62450ad1486c973f3c53947244ebe
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-02 16:44:31 +0100

    ignore file exists

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { $ } from "https://deno.land/x/dax@0.15.0/mod.ts";
+export { $, CommandBuilder  } from "https://deno.land/x/dax@0.15.0/mod.ts";
--- mold.ts
+++ mold.ts
@@ -1,4 +1,4 @@
-import { $ } from "./deps.ts";
+import { $, CommandBuilder  } from "./deps.ts";
 
 export const installMold = async () => {
   const version = await fetch(
@@ -18,7 +18,7 @@ export const installMold = async () => {
     $.fs.expandGlobSync("mold-*-x86_64-linux").next().value.name;
   await $`mv  ${downloadedMold} mold`;
 
-  await $`mkdir .cargo`;
+  await new CommandBuilder().command(["mdkir", ".cargo"]).noThrow().spawn(); // ignore file exists
 
   // cargo and cargo.toml are both valid
   // check if there is one already and use it
